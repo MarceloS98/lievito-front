730a48305f654dbe130d225095a28b7d5b31675e
Marcelo Sosa <sosamarcelo92@gmail.com>
2023-11-08 18:19:30 -0300

    change folder structure of components

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,5 +1,5 @@
 export { default as Header } from '$lib/components/Header.svelte';
 export { default as Card } from '$lib/components/Card.svelte';
-export { default as Table } from '$lib/components/Table/Table.svelte';
+export { default as Table } from '$lib/components/Table.svelte';
 export { default as CreateProductModal } from '$lib/components/CreateProductModal.svelte';
 export { default as CreateIngredientModal } from '$lib/components/CreateIngredientModal.svelte';
